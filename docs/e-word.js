64f5cce54f6f083bec2faab86b452b92a42c39eb
Hiromitsu Yamaguchi <yamagame@mac.com>
2019-09-17 20:31:52 +0900

    add YES NO buttons and time.

--- docs/e-word.js
+++ docs/e-word.js
@@ -1,16 +1,40 @@
 var n = 0;
+var a = 0;
+var f = false;
+var tim = 0;
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
-function goNextWord() {
+function setWord(w) {
   var word = document.getElementById("word");
-  word.innerHTML = words[n];
+  word.innerHTML = w;
   word.setAttribute("href", "https://www.google.com/search?q="+encodeURI(word.innerHTML+' 意味'));
-  // word.setAttribute("href", "https://www.google.com/search?q="+encodeURI('define:'+word.innerHTML));
+  var count = document.getElementById("count");
+  count.innerHTML = a+'/'+n;
+}
+
+function goNextWord(type) {
+  if (type === 'YES') {
+    a ++;
+  }
+  const w = words[n];
   n ++;
   if (n >= word.length) n = 0;
+  setWord(w);
+  if (!f) {
+    setInterval(() => {
+      tim ++;
+      var time = document.getElementById("time");
+      if (tim >= 60*60) {
+        time.innerHTML = ('00'+parseInt((tim/(60*60)) % 60)).slice(-2)+':'+('00'+parseInt((tim/60) % 60)).slice(-2)+':'+('00'+(tim % 60)).slice(-2);
+      } else {
+        time.innerHTML = ('00'+parseInt((tim/60) % 60)).slice(-2)+':'+('00'+(tim % 60)).slice(-2);
+      }
+    }, 1000);
+    f = true;
+  }
 }
 
 for (var i=0;i<words.length;i++) {
@@ -20,3 +44,5 @@ for (var i=0;i<words.length;i++) {
   words[a] = words[b];
   words[b] = c;
 }
+
+setWord('English');
